890791fe95d05415c5771e5bc9cdbf593104e762
Emmanuel Sekyi <emmanuelkwamesekyi@gmail.com>
2022-01-23 00:45:56 +0200

    added created_on and modified_on abstract models

--- task/models.py
+++ task/models.py
@@ -1,5 +1,13 @@
 from django.db import models
-
+from django.utils.timezone import now
 # Create your models here.
-class Task(models.Model):
+class BaseModel(models.Model):
+    created_date = models.DateTimeField(default=now,editable=False)
+    modified_date = models.DateTimeField(default=now, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class Task(BaseModel):
     title = models.CharField(max_length=200)
