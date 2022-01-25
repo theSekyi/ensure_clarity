1d1404f64bffff53e986f6167623b2f45c3d6a82
Emmanuel Sekyi <emmanuelkwamesekyi@gmail.com>
2022-01-25 23:21:32 +0200

    nested serializer

--- task/serializers.py
+++ task/serializers.py
@@ -1,14 +1,27 @@
 from rest_framework import serializers
 from .models import Task,SubTask
 
-class TaskSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Task
-        fields = '__all__'
 
 class SubTaskSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = SubTask
         fields = '__all__'
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    sub_task = SubTaskSerializer(many=True)
+
+    def create(self, validated_data):
+        temp_task_details = validated_data.pop('sub_task')
+        new_task = Task.objects.create(**validated_data)
+        for i in temp_task_details:
+            SubTaskSerializer.objects.create(**i, task=new_task)
+        return new_task
+
+
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+
