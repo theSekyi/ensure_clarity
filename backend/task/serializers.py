3fc2580752ab1ce589319b9b0679e65d53f92a42
Emmanuel Sekyi <emmanuelkwamesekyi@gmail.com>
2022-01-26 22:25:33 +0200

    moved files to backend folder

--- backend/task/serializers.py
+++ backend/task/serializers.py
@@ -12,14 +12,6 @@ class SubTaskSerializer(serializers.ModelSerializer):
 class TaskSerializer(serializers.ModelSerializer):
     main_task = SubTaskSerializer(many=True)
 
-    def create(self, validated_data):
-        temp_task_details = validated_data.pop('sub_task')
-        new_task = Task.objects.create(**validated_data)
-        for i in temp_task_details:
-            SubTaskSerializer.objects.create(**i, task=new_task)
-        return new_task
-
-
     class Meta:
         model = Task
         fields = '__all__'
